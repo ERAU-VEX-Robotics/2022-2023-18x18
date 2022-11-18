fbc7f428014ccb6e761372cab94dafabd330ef51
Nikhil Dave <nikhilrdave2003@gmail.com>
2022-11-17 19:33:17 -0700

    Initialized Intake class We're now ready to test the intake whenever build team is done with it

--- include/externs.hpp
+++ include/externs.hpp
@@ -2,7 +2,9 @@
 #define EXTERNS_HPP
 
 #include "Drivetrain.hpp"
+#include "Intake.hpp"
 
 extern Drivetrain drive;
+extern Intake intake;
 
 #endif /* externs.hpp */
--- src/initialize.cpp
+++ src/initialize.cpp
@@ -3,6 +3,7 @@
 Drivetrain drive({20, 19, 18, 17, 16}, {10, 9, 8, 7, 6},
                  {true, false, true, false, true},
                  {false, true, false, true, false});
+Intake intake({3, 4}, {false, false});
 /**
  * Runs initialization code. This occurs as soon as the program is started.
  *
