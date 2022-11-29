301028725ebaa7dd5cb379ffa805c747c9fdb482
Nikhil Dave <nikhilrdave2003@gmail.com>
2022-11-28 18:47:43 -0700

    Created Flywheel.hpp, with all function declarations

---
+++ include/Flywheel.hpp
@@ -0,0 +1,69 @@
+#ifndef FLYWHEEL_HPP
+#define FLYWHEEL_HPP
+
+/**
+ * \file Flywheel.hpp
+ *
+ * This file contains the class declaration for the Flywheel class.
+ * This class represents the physical flywheel used to launch the
+ * disks
+ */
+#include "Motor_Group.hpp"
+#include <initializer_list>
+
+class Flywheel {
+  private:
+    // The motor group containing all of the motors on the flywheel
+    Motor_Group motors;
+
+  public:
+    /**
+     * The Constructor for the Intake Class
+     *
+     * \param ports A list of ports used by motors on the flywheel
+     * \param reverses A list of booleans specifying which motors are reversed
+     *             Each element in the list corresponds to the respective
+     *             element in ports, i.e. whether or not the motor in the port
+     *             specified in the third index of ports is determined by the
+     *             third index of revs.
+     */
+    Flywheel(std::initializer_list<int> ports,
+             std::initializer_list<bool> reverses);
+
+    /**
+     * Function: set_velocity
+     *
+     * This function sets the target velocity for the flywheel.
+     * This implementation sets all the motors to the same velocity
+     */
+    void set_velocity(int16_t velocity);
+
+    /**
+     * Function: set_velocity
+     *
+     * This function sets the target velocity for the flywheel.
+     * This implementation sets each motor's velocity individually
+     */
+    void set_velocity(std::vector<int16_t> velocity);
+
+    /**
+     * Function: stop
+     *
+     * This function stops the flywheel.
+     */
+    void stop();
+
+    /**
+     * Function: print_telemetry
+     *
+     * This function allows for the printing of various telemetry information
+     * from the motors in the Motor_Group. It wraps the call to
+     * Motor_Group::print_telemetry. See that function for more information
+     *
+     * \param vals_to_print A bitfield specifying which values to print. See
+     *                      Motor_Group::print_telemetry for more information
+     */
+    void print_telemetry(uint8_t vals_to_print);
+};
+
+#endif /* Flywheel.hpp */
