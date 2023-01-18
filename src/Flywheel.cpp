866305cb0584f68a3d62c8c5a0b174b811f3618d
Nikhil Dave <nikhilrdave2003@gmail.com>
2023-01-17 19:56:58 -0700

    Set up separate task to run a PID controller for the Flywheel

--- src/Flywheel.cpp
+++ src/Flywheel.cpp
@@ -1,5 +1,8 @@
 #include "Flywheel.hpp"
-#include "pros/motors.h"
+#include "pros/rtos.h"
+#include "utils.h"
+#include <cmath>
+#include <iostream>
 
 Flywheel::Flywheel(std::initializer_list<int> ports,
                    std::initializer_list<bool> reverses)
@@ -11,11 +14,52 @@ Flywheel::Flywheel(std::initializer_list<int> ports,
      * of 3600 from the motor. However, this is not an option to set the motor's
      * gearing to, so I approximate using the maximum internal motor cartridge,
      * which is 600 RPM. This value is important for the move_velocity function
-     * used to run the motors. This function is used because it includes an
-     * internal PID
+     * used to run the motors, in case I need to use that function.
      */
     motors.set_gearing(pros::E_MOTOR_GEAR_BLUE);
     motors.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+}
+
+void Flywheel::pid_task_fn() {
+    double integral = 0;
+    double prev_error = 0;
+    double error = 0;
+
+    while (1) {
+        error = flywheel_velo - motors.get_avg_velocity();
+        double voltage = pid(kP, kI, kD, error, &integral, &prev_error);
+
+        if (fabs(voltage) > 12000)
+            voltage = copysign(12000, voltage);
+
+        std::cout << "Motor Voltage: " << voltage << std::endl;
+        std::cout << "Motor Velocity: " << motors.get_avg_velocity()
+                  << std::endl;
+
+        motors.move_voltage(voltage);
+        pros::delay(20);
+    }
+}
+
+void Flywheel::trampoline(void *param) {
+    if (param) {
+        Flywheel *that = static_cast<Flywheel *>(param);
+        that->pid_task_fn();
+    }
+}
+
+void Flywheel::init_pid_task() {
+    pid_task =
+        pros::c::task_create(trampoline, this, TASK_PRIORITY_DEFAULT,
+                             TASK_STACK_DEPTH_DEFAULT, "Flywheel PID Task");
+}
+
+void Flywheel::end_pid_task() { pros::c::task_delete(pid_task); }
+
+void Flywheel::set_pid_consts(double Pconst, double Iconst, double Dconst) {
+    kP = Pconst;
+    kI = Iconst;
+    kD = Dconst;
 }
 
 void Flywheel::driver(pros::controller_id_e_t controller,
