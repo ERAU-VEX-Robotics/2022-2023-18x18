6b363248b76210d31a2c8f31006aaa37e4d9e036
Nikhil Dave <nikhilrdave2003@gmail.com>
2023-01-31 18:53:26 -0700

    Added planned autonomous route for the drivetrain

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -13,8 +13,40 @@
  * from where it left off.
  */
 void autonomous() {
-    flywheel.resume_pid_task();
+    // flywheel.resume_pid_task();
     drive.init_pid_task();
-    flywheel.pause_pid_task();
+    // flywheel.pause_pid_task();
+
+    // Move to grab disk in front of bot
+    drive.move_straight(48);
+    drive.wait_until_settled();
+
+    // Aim and Fire 3 disks
+    drive.turn_angle(25);
+    drive.wait_until_settled();
+
+    drive.move_straight(10);
+    drive.wait_until_settled();
+
+    // Grab a few more disks
+    drive.turn_angle(-125);
+    drive.wait_until_settled();
+
+    drive.move_straight(16);
+    drive.wait_until_settled();
+
+    drive.turn_angle(40);
+    drive.wait_until_settled();
+
+    drive.move_straight(34);
+    drive.wait_until_settled();
+
+    // Aim to fire more disks
+    drive.turn_angle(100);
+    drive.wait_until_settled();
+
+    drive.move_straight(10);
+    drive.wait_until_settled();
+
     drive.end_pid_task();
 }
