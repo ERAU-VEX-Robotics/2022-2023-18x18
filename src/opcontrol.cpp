9eb6c24460d053ad7a2e7e54dfa04865d6d142f4
Nikhil Dave <nikhilrdave2003@gmail.com>
2023-02-03 19:42:41 -0700

    Changed drive to use right joystick, using arcade_driver

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -19,7 +19,7 @@ void opcontrol() {
     while (true) {
         flywheel.driver(pros::E_CONTROLLER_MASTER, pros::E_CONTROLLER_DIGITAL_A,
                         pros::E_CONTROLLER_DIGITAL_B);
-        drive.arcade_driver(pros::E_CONTROLLER_MASTER);
+        drive.arcade_driver(pros::E_CONTROLLER_MASTER, true);
         intake.driver(pros::E_CONTROLLER_MASTER, pros::E_CONTROLLER_DIGITAL_R1,
                       pros::E_CONTROLLER_DIGITAL_L1,
                       pros::E_CONTROLLER_DIGITAL_R2);
