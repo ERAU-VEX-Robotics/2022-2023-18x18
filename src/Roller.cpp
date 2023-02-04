1c8abae9021e993b3410a440b1e7e12fbddfca6b
Nikhil Dave <nikhilrdave2003@gmail.com>
2023-02-03 19:30:48 -0700

    Added driver function

--- src/Roller.cpp
+++ src/Roller.cpp
@@ -1,5 +1,4 @@
 #include "Roller.hpp"
-#include "pros/motors.h"
 
 Roller::Roller(std::initializer_list<int> ports,
                std::initializer_list<bool> revs, double gear_ratio)
@@ -16,4 +15,15 @@ void Roller::clockwise(double degrees) {
 
 void Roller::counterclockwise(double degrees) {
     motors.move_relative(-degrees / gear_ratio, 200);
+}
+
+void Roller::driver(pros::controller_id_e_t controller,
+                    pros::controller_digital_e_t cw_btn,
+                    pros::controller_digital_e_t ccw_btn) {
+    if (pros::c::controller_get_digital(controller, cw_btn))
+        clockwise();
+    else if (pros::c::controller_get_digital(controller, ccw_btn))
+        counterclockwise();
+    else
+        stop();
 }
