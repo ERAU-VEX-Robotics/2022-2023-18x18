04145fbf5dfe4d0621e9b147b1baa99ffded2d60
Nikhil Dave <nikhilrdave2003@gmail.com>
2023-02-22 17:51:00 -0700

    Grabbed small bot intake code - new intake is identical The new intake design for the 18x18 robot functions identically to the 15x15 robot's intake, so I just copied the class declaration and definition over to the 18x18 robot's code

--- include/Intake.hpp
+++ include/Intake.hpp
@@ -14,28 +14,21 @@
 
 class Intake {
   private:
-    // bot_mtrs: the motors powering the half of the intake that gets disks from
-    // the ground.
-    // top_mtrs: The motors that moves disks into the flywheel
-    Motor_Group bot_mtrs, top_mtrs;
+    Motor_Group motors;
 
   public:
     /**
      * The Constructor for the Intake Class
      *
-     * \param bot_ports A list of ports used by the bottom motors on the intake
-     * \param bot_reverses A list of booleans specifying which bot motors are
-     * reversed Each element in the list corresponds to the respective element
-     * in ports, i.e. whether or not the motor in the port specified in the
-     * third index of ports is determined by the third index of revs.
-     * \param top_ports A list of ports used by the bottom motors on the intake
-     * \param top_reverses A list of booleans specifying which top motors are
-     * reversed
+     * \param ports A list of ports used by motors on the intake
+     * \param reverses A list of booleans specifying which motors are reversed
+     *             Each element in the list corresponds to the respective
+     *             element in ports, i.e. whether or not the motor in the port
+     *             specified in the third index of ports is determined by the
+     *             third index of revs.
      */
-    Intake(std::initializer_list<int> bot_ports,
-           std::initializer_list<bool> bot_reverses,
-           std::initializer_list<int> top_ports,
-           std::initializer_list<bool> top_reverses);
+    Intake(std::initializer_list<int> ports,
+           std::initializer_list<bool> reverses);
 
     /**
      * Function: driver
@@ -43,26 +36,24 @@ class Intake {
      * The function used to control the intake in driver control
      * \param controller The controller ID whose buttons to read
      * \param in_button The digital button ID to press to run the motors to
-     *                  collect disks and push them into the flywheel
+     *                  collect disks
      * \param out_button The digital button ID to press to run the motors to
      *                   expel disks
-     * \param in_top_button The digital button ID to only run the top motors to
-     * push disks into the flywheel
      */
     void driver(pros::controller_id_e_t controller,
                 pros::controller_digital_e_t in_button,
-                pros::controller_digital_e_t out_button,
-                pros::controller_digital_e_t in_top_button);
+                pros::controller_digital_e_t out_button);
 
     // Wraps the necessary code to run the motors to collect disks
-    void in(void);
+    void in();
     // Wraps the necessary code to run the motors to expel disks from the intake
-    void out(void);
-    // Wraps the necessary code to run only the top motors to push disks into
-    // the flywheel
-    void in_top(void);
-    // Stops the intake
-    void stop(void);
+    void out();
+    // Wraps the necessary code to stop the motors
+    void stop();
+
+    // Rotates the intake a specific number of degrees. Used for the attached
+    // roller rotation mechanism.
+    void turn_degree(double degrees);
 
     /**
      * Function: print_telemetry
