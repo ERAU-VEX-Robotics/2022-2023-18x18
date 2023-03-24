6b43aede6a31ea715a86d7c35c02b58c80cf6615
Nikhil Dave <nikhilrdave2003@gmail.com>
2023-03-23 18:39:57 -0700

    Added auton_id variable to externs

--- include/externs.hpp
+++ include/externs.hpp
@@ -3,14 +3,17 @@
 
 #include "Drivetrain.hpp"
 #include "Flywheel.hpp"
+#include "Indexer.hpp"
 #include "Intake.hpp"
 #include "Roller.hpp"
-#include "Indexer.hpp"
+#include "gui.h"
 
 extern Drivetrain drive;
 extern Intake intake;
 extern Flywheel flywheel;
 extern Roller roller;
 extern Indexer indexer;
+
+extern enum auton auton_id;
 
 #endif /* externs.hpp */
